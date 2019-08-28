ccc504a9aad7aeafff1746b9caa06934a0a08cdb
Seblee <ceeu@vip.qq.com>
2019-08-28 10:12:11 +0800

    add clang-format file;

--- USER_PROGRAM/USER_PROGRAM_01.C
+++ USER_PROGRAM/USER_PROGRAM_01.C
@@ -1,4 +1,4 @@
-#define _CCV3_ // 定义C编译器版本.. --CCG 注释
+#define _CCV3_  // 定义C编译器版本.. --CCG 注释
 
 #include "..\TKS_GLOBE_VARIES.H"
 #include "..\MAIN_PROGRAM_V100\MAIN_PROGRAM_V100.H"
@@ -30,7 +30,7 @@ volatile uchar r_temp_bak, r_temp;
 //**********************************************
 //==============================================
 //#pragma vector  Interrupt_Extemal        @ 0x04
-//void Interrupt_Extemal()
+// void Interrupt_Extemal()
 
 void __attribute((interrupt(0x04))) Interrupt_Extemal(void)
 {
@@ -42,7 +42,7 @@ void __attribute((interrupt(0x04))) Interrupt_Extemal(void)
 // CTM0 CCRP INTERRUPT
 //==============================================
 //#pragma vector  Interrupt_CTM0_CCRP        @ 0x10
-//void Interrupt_CTM0_CCRP()
+// void Interrupt_CTM0_CCRP()
 
 void __attribute((interrupt(0x10))) Interrupt_CTM0_CCRP(void)
 {
@@ -55,11 +55,10 @@ void __attribute((interrupt(0x10))) Interrupt_CTM0_CCRP(void)
 // 250uS---定时器
 //==============================================
 //#pragma vector  Interrupt_CTM0_CCRA       @ 0x14
-//void Interrupt_CTM0_CCRA()
+// void Interrupt_CTM0_CCRA()
 void __attribute((interrupt(0x14))) Interrupt_CTM0_CCRA(void)
 {
-
-    GCC_CLRWDT(); //_clrwdt();
+    GCC_CLRWDT();  //_clrwdt();
 }
 
 //==============================================
@@ -67,11 +66,11 @@ void __attribute((interrupt(0x14))) Interrupt_CTM0_CCRA(void)
 // PTM1 CCRP INTERRUPT
 //==============================================
 //#pragma vector  Interrupt_PTM1_CCRP       @ 0x18
-//void Interrupt_PTM1_CCRP()
+// void Interrupt_PTM1_CCRP()
 
 void __attribute((interrupt(0x18))) Interrupt_PTM1_CCRP(void)
 {
-    GCC_NOP(); //_nop();
+    GCC_NOP();  //_nop();
 }
 
 //==============================================
@@ -79,7 +78,7 @@ void __attribute((interrupt(0x18))) Interrupt_PTM1_CCRP(void)
 // PTM1 CCRA INTERRUPT
 //==============================================
 //#pragma vector  Interrupt_PTM1_CCRA        @ 0x1C
-//void Interrupt_PTM1_CCRA()
+// void Interrupt_PTM1_CCRA()
 
 void __attribute((interrupt(0x1c))) Interrupt_PTM1_CCRA(void)
 {
@@ -89,7 +88,7 @@ void __attribute((interrupt(0x1c))) Interrupt_PTM1_CCRA(void)
 // TB1 INTERRUPT
 //==============================================
 //#pragma vector  Interrupt_TB1      @ 0x20
-//void Interrupt_TB1()
+// void Interrupt_TB1()
 
 void __attribute((interrupt(0x20))) Interrupt_TB1(void)
 {
@@ -100,18 +99,17 @@ void __attribute((interrupt(0x20))) Interrupt_TB1(void)
 //==============================================
 void USER_PROGRAM_INITIAL()
 {
-
-    _pac = 0b11111111;
+    _pac  = 0b11111111;
     _papu = 0b11111111;
-    _pa = 0b11111111;
+    _pa   = 0b11111111;
 
-    _pbc = 0b00001111;
+    _pbc  = 0b00001111;
     _pbpu = 0b00000000;
-    _pb = 0b00000000;
+    _pb   = 0b00000000;
 
-    _pcc = 0b00000000;
+    _pcc  = 0b00000000;
     _pcpu = 0b00000000;
-    _pc = 0b00000000;
+    _pc   = 0b00000000;
 
     _lvdc = 0x00;
 
@@ -121,15 +119,15 @@ void USER_PROGRAM_INITIAL()
     _eea = 0x00;
     _eed = 0x00;
 
-    _i2ctoc = 0x00;
-    _iicc0 = 0x00;
-    _iicc1 = 0x00;
-    _iicd = 0x00;
-    _iica = 0x00;
-    _usr = 0x00;
-    _ucr1 = 0x00;
-    _ucr2 = 0x00;
-    _brg = 0x00;
+    _i2ctoc  = 0x00;
+    _iicc0   = 0x00;
+    _iicc1   = 0x00;
+    _iicd    = 0x00;
+    _iica    = 0x00;
+    _usr     = 0x00;
+    _ucr1    = 0x00;
+    _ucr2    = 0x00;
+    _brg     = 0x00;
     _txr_rxr = 0x00;
 
     _tmpc = 0x00;
@@ -150,32 +148,31 @@ void USER_PROGRAM_INITIAL()
 //==============================================
 void USER_PROGRAM()
 {
-
     if (SCAN_CYCLEF)
     {
         GET_KEY_BITMAP();
         r_temp = DATA_BUF[0];
-        /*	 	    if(r_temp==r_temp_bak) 
-			{   
-			  	if(f_KeyDown==1)
-			  	{
-			  	  f_KeyDown = 0;
-				  if(r_temp_bak&0x01)
-				  LED1 =! LED1;
-				  if(r_temp_bak&0x02)
-				  LED2 =! LED2 ;			  
-				  if(r_temp_bak&0x04)
-				  LED3 =! LED3 ;			  
-				  if(r_temp_bak&0x08)
-				  LED4 =! LED4;
-				 
-			  	}		  	 
-			}
-			else
-			{
-			  	r_temp_bak = r_temp;
-			  	f_KeyDown=1;
-			}*/
+        /*	 	    if(r_temp==r_temp_bak)
+            {
+                if(f_KeyDown==1)
+                {
+                  f_KeyDown = 0;
+                  if(r_temp_bak&0x01)
+                  LED1 =! LED1;
+                  if(r_temp_bak&0x02)
+                  LED2 =! LED2 ;
+                  if(r_temp_bak&0x04)
+                  LED3 =! LED3 ;
+                  if(r_temp_bak&0x08)
+                  LED4 =! LED4;
+
+                }
+            }
+            else
+            {
+                r_temp_bak = r_temp;
+                f_KeyDown=1;
+            }*/
         if (r_temp != r_temp_bak)
         {
             if (r_temp & 0x01)
